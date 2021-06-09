9091b60f43b56150898220babdff3c5e27950eac
Colin Morrison <colinjmmorris@gmail.com>
2021-06-09 22:19:46 +0100

    MainContainer handlePost function added

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+    return(
+        <header>
+
+        <ul>
+            <li className="navBarItem">
+            <Link to="/plots">Plots</Link>
+            </li>
+            <li className="navBarItem">
+            <Link to="/knowhows">Know Hows</Link>
+            </li>
+            <li className="navBarItem">
+            <Link to="/community">Community</Link>
+            </li>
+        </ul>
+
+        </header>
+    )
+}
+
+export default NavBar
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import HomePageContainer from './HomePageContainer';
 import Request from '../helpers/request';
+import NavBar from '../components/NavBar';
 
-
-
-// Responsible for State and RequestAlls from DB
-
+// This container is responsible for State, initial requests to DB to GET, and other requests (post new etc.)
 // Renders HomePageContainer once user is logged in
-
 // Props passed down: all, to HomePageContainer (we also pass 1 user here. The currentUser who has logged in, methodology TBC..)
 
 const MainContainer = () =>{
-    const [currentUser, setCurrentUser] = useState([]);
+    const [currentUser, setCurrentUser] = useState([]); // Currently no user is set. Our Login function should setCurrentUser
     const [plots, setPlots] = useState([]);
     const [knowHows, setKnowHows] = useState([]);
     const [bulletins, setBulletins] = useState([]);
@@ -37,17 +36,87 @@ const MainContainer = () =>{
                 setBulletins(data[2]);
                 setJobs(data[3]);
                 setTips(data[4]);
-                // Likewise as above, may need one for currentUser
+                // Likewise as above, may need one of these for currentUser if promised
             })}
 
     useEffect(()=>{requestAll()}, [])
 
-    return(
-        <>
-            <h1>This is the Main Container</h1>
-            {/* If user logged in, render HomePageContainer. Else render Login screen */}
 
+    // We currently need this handlePost for knowHow, Jobs, Bulletins etc. Can we condense it to one, via object type?
+            // Means we only need to pass ONE prop down instead of 3. As posters should go in main container
+    // Also could just have them in their relevant files, but requests SHOULD go in main container
+    const handlePostKnowHow = (knowHow) => {
+        const request = new Request();
+        request.post("/knowHow", knowHow)
+        .then(() => window.location = '/knowHows') //Not reaally sure what this does
+    }
+
+    const handlePostBulletin = (bulletin) => {
+        const request = new Request();
+        request.post("/bulletin", bulletin)
+        .then(() => window.location = '/bulletins') 
+    }
+
+    const handlePostJob = (job) => {
+        const request = new Request();
+        request.post("/job", job)
+        .then(() => window.location = '/jobs')
+    }
+
+    // Trialling what the above refactor would look like:
+        // Problem is, we don't have an Object to compare it to
+        // so can't do "if newObject is instance of knowHow"
+        // Trying: "if new Object is instance of knowHows[0]" ?? 
+
+    const handlePost = (newObject) => {
+        const request = new Request();
+        if (newObject instanceof knowHows[0]){
+            request.post("/knowHow", knowHow)
+            .then(() => window.location = '/knowHows')
+        }
+        if (newObject instanceof jobs[0]){
+            request.post("/job", job)
+            .then(() => window.location = '/jobs')
+        }
+        if (newObject instanceof bulletins[0]){
+            request.post("/bulletin", bulletin)
+            .then(() => window.location = '/bulletins') 
+        }
+    }
+
+
+    return(
+        <Router>
+        <> 
+            <NavBar/> 
+
+            <h1>This is the Main Container</h1>
+
+             {/* If user logged in, render HomePageContainer. Else render Login screen. 
+                Easiest way is to make a Login.js component to render?
+                Can we have if statements in our Switches to check the state of currentUser?
+                */}
+
+
+            <Switch>
+
+                <Route path="/home" render={() => {
+                    return(
+                        <HomePageContainer 
+                            currentUser = {currentUser}
+                            plots = {plots}
+                            knowHows = {knowHows}
+                            bulletins = {bulletins}
+                            jobs = {jobs}
+                            tips = {tips}
+                            // also pass in the handlePost method once working. 
+                            // And handleEdit and handleDelete once onto extensions
+                        />)
+                }}/>
+
+        </Switch>
         </>
+        </Router>
     )
 
 }
