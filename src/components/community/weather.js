6577846fdeafd2d90c6a77d6e51f3d895b7b8f6e
Andrew Sharkey <sharkand@hotmail.com>
2021-06-12 23:37:43 +0100

    Some refactoring of weather info.

--- src/components/community/weather.js
+++ src/components/community/weather.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 const Weather = ({weatherData}) => {
     const location = "Glasgow";
+    const description = weatherData.weather[0].description;
+    const max = Math.round(weatherData.main.temp_max) - 273;
+    const min = Math.round(weatherData.main.temp_min) -273;
 
     return(
     <div id="weather-widget">
-       <h1>Weather Data</h1>
+        {
+        (weatherData.weather[0].main === "")? (<h1>Loading weather...</h1>):
+        <>
+        <h1>Weather</h1>
         <h2>{location}</h2>
-       <h2>{weatherData.weather[0].description}</h2>
-
-       <h3><span>Max: {Math.round(weatherData.main.temp_max) - 273}C</span> <span>Min: {Math.round(weatherData.main.temp_min) -273}C</span></h3>
+        <h2>{description}</h2>
+       <h3><span>Max: {max}C</span> <span>Min: {min}C</span></h3>)
+       </>
+        }
      </div>
-    )
+    )   
 };
 
 export default Weather;
