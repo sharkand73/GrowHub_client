675aa25198d7ccbb13b6937ded3376afe3ee3fd9
Colin Morrison <colinjmmorris@gmail.com>
2021-06-13 17:11:09 +0100

    Jobs rendering by due date

--- src/components/community/job/JobList.js
+++ src/components/community/job/JobList.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Job from './Job.js';
 import {Link} from 'react-router-dom';
 
 const JobList = ({jobs, currentUser}) =>{
 
-// Sort jobs into reverse date order (newest up top)
-const jobsInDateOrder = jobs.sort((a, b) => {
-    a = a.date.split('/').reverse().join('');
-    b = b.date.split('/').reverse().join('');
-    return a > b ? -1 : a < b ? 1 : 0;
+// Sorts jobs by deadline date, most urgent at top
+const jobsByDeadline = jobs.sort((a, b) => {
+    a = a.deadline.split('/').reverse().join('');
+    b = b.deadline.split('/').reverse().join('');
+    return a < b ? -1 : a > b ? 1 : 0;
 });
 
-const jobArray = jobsInDateOrder.map((job, index) => {
+const jobsRenderedByDeadline = jobsByDeadline.map((job, index) => {
     return(
         <li key={index}><Job job={job} currentUser={currentUser}/></li>
     )
-})
+});
 
 return (
     <>
         <h1>This is the list of jobbies</h1>
+        
         <ul>
-            {jobArray}
+            {jobsRenderedByDeadline}
         </ul>
 
         <button>
