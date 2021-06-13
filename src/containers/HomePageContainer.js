3b81cb9c643e5279fc4d2060d10dd04d72e7ded2
Colin Morrison <colinjmmorris@gmail.com>
2021-06-13 16:16:33 +0100

    Top 3 committee bulletins showing in date order

--- src/containers/HomePageContainer.js
+++ src/containers/HomePageContainer.js
@@ -5,45 +5,33 @@ import Bulletin from '../components/community/bulletin/Bulletin';
 import Tip from '../components/community/tip/Tip';
 import Weather from '../components/community/weather';
 
-
-// Renders weather widgets etc
-// Renders buttons for areas of the site
-// Renders some recent news bulletins (requires loop, find 3 most recent from bulletins prop, or something)
-// Renders tip of the month
-// Renders "hello {user}" message
-
-// Props to pass down:
-    // Plots = Plots & User
-    // KnowHows = KnowsHows & User
-    // Community = Bulletins, Jobs, User
-
-
-// OTHER INFO:
-    // It will probably be easier to actually make separate components for weather widgets, quick bulletins, tips etc
-    // Makes the Rendering a bit simpler. Don't need to have all the HTML here under one giant conditional render
-
 const HomePageContainer = ({currentUser, bulletins, tips, weatherData, getDate}) =>{
+    
+    // Sort the bulletins via reverse date order
+    const bulletinsInDateOrder = bulletins.sort((a, b) => {
+        a = a.date.split('/').reverse().join('');
+        b = b.date.split('/').reverse().join('');
+        return a > b ? -1 : a < b ? 1 : 0;
+    });
 
-    const committeeBulletins = bulletins.map((bulletin, index) => {
-            if (bulletin.author.position !== "NONE" || "INACTIVE"){
-                return(
-                    <li key={index}><Bulletin bulletin={bulletin} currentUser={currentUser}/></li>
-                )
-            }
-            return null;
-        })
+    // Filter out non-committee member posts
+    const committeeBulletins = bulletinsInDateOrder.filter(bulletin => bulletin.author.position !== "NONE" || "INACTIVE");
 
+    // Slice the most recent 3
+    const threeBulletins = committeeBulletins.slice(0,3);
+
+    // Map through the threeBulletins to create an li element
+    const bulletinsForRender = threeBulletins.map((bulletin, index) => {
+        return <li key={index}><Bulletin bulletin={bulletin} currentUser={currentUser}/></li>
+    })
+    
     return(
         <>
         <h2>This is our home page container</h2>
-        {/* 
-            We want to render our widgets etc here.
-            And we want them conditionally 
-            */}
 
         <div>
             <ul>
-            {committeeBulletins}
+                {bulletinsForRender}
             </ul>
 
             <div>
