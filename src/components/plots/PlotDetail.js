c68c4d916016b0adeb95f99e25e3a311cb2b6eb1
Andrew Sharkey <sharkand@hotmail.com>
2021-06-15 01:14:47 +0100

    Added detail to individual plot page.

--- src/components/plots/PlotDetail.js
+++ src/components/plots/PlotDetail.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 const PlotDetail = ({currentUser, plot}) =>{
 
+    const getPlotHolders = () => {
+    let plotHolders = "";
+    let i = 0;
+    for(let user of plot.users){
+        if (i>0){plotHolders += ", "};
+        plotHolders += user.shortName;
+        i++;
+    }
+    return plotHolders;
+    }
+
+    console.log(plot.areaName);
+    console.log(plot.users);
 
     return(
         <>
-            <h3>This is the plot detail page</h3>
+            <h3>Details for {plot.areaName}</h3>
             <ul>
                 <li>Plot Number: {plot.plotNumber}</li>
                 <li>Dimensions: {plot.length}m x {plot.breadth}m</li>
-                <li> {plot.isFlat ? "This plot is flat!" : "This plot is as flat as planet Earth (not flat)"}</li>
+                <li>Inclination: {plot.isFlat ? "flat" : "slope"}</li>
+                <li>Plotholders: {getPlotHolders()}</li>
             </ul>
+            <h3>Plot history:</h3>
+            <p>Inserts plot comments here</p>
         </>
     )
 
