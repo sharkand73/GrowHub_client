3d36d928d6a1444064baf3da9dfb79a39d0a2adf
Andrew Sharkey <sharkand@hotmail.com>
2021-06-15 21:18:44 +0100

    Weather widget aligned.

--- src/components/community/weather.js
+++ src/components/community/weather.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import '../../css/weather.css';
 
 const Weather = ({weatherData}) => {
     const location = "Glasgow";
     const description = weatherData.weather[0].description;
     const max = Math.round(weatherData.main.temp_max) - 273;
     const min = Math.round(weatherData.main.temp_min) - 273;
-    const iconCode = weatherData.weather[0].icon;
-    //const weatherIcon = require("http://openweathermap.org/img/w"+iconCode).default;
+    const iconCode = weatherData.weather[0].icon+".png";
+    const weatherIcon = "http://openweathermap.org/img/w/"+iconCode;
     return(
     <div id="weather-widget">
         {
         (weatherData.weather[0].main === "")? (<h1>Loading weather...</h1>):
-        <>
+        <div id="widget-container">
         <h1>Weather</h1>
-
         <h3>{location}</h3>
-        <img src="http://openweathermap.org/img/w/10d.png"/>
-        <p class="dash-text">Outlook: {description}</p>
-        <p class="dash-text"><span>Max: {max}&#176;C</span> <span>Min: {min}&#176;C</span></p>
+        <div id="image-temp">
+            <img id="widget-image" src={weatherIcon}/>
+            <div id="temperature">
+                <p class="dash-text" id="max-temp">
+                    {max}&#176;C
+                </p>
+                <p class="dash-text" id="min-temp">
+                    {min}&#176;C
+                </p>
+            </div>
+        </div>
+        <div class="dash-text">{description}</div>
+        
 
-       </>
+       </div>
         }
      </div>
     )   
