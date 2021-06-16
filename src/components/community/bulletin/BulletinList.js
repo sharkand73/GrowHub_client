2f5e17ff9825afdb6acedf8fb9e2ea042cd6e5ed
Andrew Sharkey <sharkand@hotmail.com>
2021-06-16 12:40:23 +0100

    Bulletin edit styled to perfection.

--- src/components/community/bulletin/BulletinList.js
+++ src/components/community/bulletin/BulletinList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Bulletin from './Bulletin.js';
 import EditBulletin from './EditBulletin.js';
+import {Link} from 'react-router-dom';
 
 // The purpose of this file is to render a list of bullletins and also render the 
 // new bulletin file
@@ -16,18 +17,26 @@ const BulletinList = ({currentUser, sortedBulletins, deleteBulletin, displayBull
 const bulletinArray = sortedBulletins.map((bulletin, index) => {
     return(
         
-        <li className="bulletin-postit" key={index}><Bulletin bulletin={bulletin} currentUser={currentUser} deleteBulletin={deleteBulletin} displayBulletin={displayBulletin} editBulletin={editBulletin} /></li>
+        <li className="postit" key={index}><Bulletin bulletin={bulletin} currentUser={currentUser} deleteBulletin={deleteBulletin} displayBulletin={displayBulletin} editBulletin={editBulletin} /></li>
     )
 })
 
 
 
 return (
+<>
     <div id = "noticeboard-list">
         <ul>
             {bulletinArray}
         </ul>
+        <h2>
+            <Link to='/bulletins/new' id="new-bulletin">
+                New Bulletin
+            </Link>
+        </h2>
     </div>
+    
+</>
 );
 }
 
--- src/components/community/bulletin/EditBulletin.js
+++ src/components/community/bulletin/EditBulletin.js
@@ -29,16 +29,22 @@ const EditBulletin = ({currentUser, bulletin, editBulletin, getDate, reverseEdit
     
     return(
         <>
-            <h3>Edit your Bulletin here!</h3>
+            <h2>Edit your Bulletin here!</h2>
                 <form onSubmit={handleSubmit}>
-    
-                    <label name='title'>Title:</label>
-                    <input class="field_size1" type='text' name='title' id='title' onChange={handleChange} defaultValue={bulletin.title} required />
-    
-                    <label name='body'>Your Bulletin:</label>
-                    <input class="field_size1" type='text' name='body' id='body' onChange={handleChange} defaultValue={bulletin.body} required /> 
-    
-                    <button class="createButton1" type='submit'>Submit Updated Bulletin</button>
+                    <div id="edit-bulletin-inner">
+                        <div>
+                            <label name='title' className="bulletin-label">Title: </label>
+                            <input class="field_size1" type='text' name='title' id='title' onChange={handleChange} defaultValue={bulletin.title} required />
+                        </div>
+                        <div>
+                            <label name='body' className="bulletin-label">Your Bulletin: </label>
+                            <input class="field_size1" type='text' name='body' id='body' onChange={handleChange} defaultValue={bulletin.body} required /> 
+                        </div>
+                    </div>
+                    <div className="flex-two">
+                        <button className="bulletin-button" type="button" onClick={()=>reverseEditClick()}>Back</button>
+                        <button className="bulletin-button" type='submit'>Submit Changes</button>
+                    </div>
                 </form>
         </>
         )
