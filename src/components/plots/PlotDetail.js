696bff0b796da560798104d33f962945b8cdb8a0
Colin Morrison <colinjmmorris@gmail.com>
2021-06-16 14:03:09 +0100

    Commentry fixed

--- src/components/plots/PlotDetail.js
+++ src/components/plots/PlotDetail.js
@@ -76,13 +76,6 @@ const PlotDetail = ({currentUser, plot, plots, getDate, postComment, comments}) 
 
             <div id="plot-history-grid">
 
-//                 <p class="plot-detail1">Plot history</p>
-//                 <div class="content">
-//                     {plot.comments?<ul  class="plot-detail2">{plotComments}</ul>: null}
-//                 </div>
-//             </div>
-//             <div  id="plot-details-grid" className = "plot-back" onClick = {()=>setSelectedPlot(null)}>
-
                 <NewPlotComment plot={plot} getDate={getDate} currentUser={currentUser}  postComment={postComment}/>
                 {commentsArrayLength > 0 ?
                 <>
